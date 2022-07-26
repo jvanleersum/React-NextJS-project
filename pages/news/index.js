3902b4dab9585b6b66523b36953a81a5b042a83f
Judith van Leersum <judithvleersum@gmail.com>
2022-07-26 17:37:05 +0200

    Added link to detail pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,14 @@
+import { Fragment } from "react";
+import Link from 'next/link';
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>
+  return <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li><Link href='/news/nextjs-is-great'>NextJS Is Great</Link></li>
+        <li><Link href='/news/some-other-thing'>Some Other Thing</Link></li>
+      </ul>
+    </Fragment>
 }
 
 export default NewsPage;
